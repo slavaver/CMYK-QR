ac3bbdf8c4c3ef2b0fdf663de174c3ed6b7ee926
YuraKhoroshev <72125925+YuraKhoroshev@users.noreply.github.com>
2021-06-17 16:08:59 +0300

    final

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from PIL import Image
 import numpy as np
 import cv2
+import os
 
 from def_analysis import analysis
 from transformation import srgb_cmyk, cmyk_srgb
@@ -24,7 +25,8 @@ def coder(name_img):
     size *= 29
     img = srgb_cmyk(name_img)
     np_img = np.array(img)
-    img.save('test_cmyk.tif')
+    img_result = './result/CMYK' + name_img[8:-3] + 'tif'
+    img.save(img_result)
 
     img_qr = Image.open('qrcode.png')
     img_qr = img_qr.convert('L')
@@ -32,8 +34,8 @@ def coder(name_img):
 
     cord = analysis(name_img, size)
     layer = cord[0]
-    cord_x = cord[1]
-    cord_y = cord[2]
+    cord_y = cord[1]
+    cord_x = cord[2]
     for i in range(cord_x, size+cord_x):
         for j in range(cord_y, size+cord_y):
             z = np_img_qr[i-cord_x, j-cord_y]
@@ -41,31 +43,31 @@ def coder(name_img):
                 np_img[i, j, layer] = z
 
     new_img = Image.fromarray(np_img.astype('uint8'), 'CMYK')
-    new_img.save('result.tif')
-    name_res = 'result.tif'
+    name_res = './result/result_CMYK' + name_img[8:-3] + 'tif'
+    new_img.save(name_res)
     cmyk_srgb(name_res)
     print(cord)
     print('end coder')
     return cord
 
-def decoder(cord):
-    name_img = 'result.tif'
-    img = Image.open(name_img)
-    img = srgb_cmyk(name_img)
-    np_img = np.array(img)
+def decoder(cord, name_img):
 
+    img = Image.open(name_img)
+
+    np_img = np.array(img)
     np_img_qr = np.zeros((58,58))
     np_img_qr.fill(255)
 
     size = 58
     layer = cord[0]
-    cord_x = cord[1]
-    cord_y = cord[2]
+    cord_y = cord[1]
+    cord_x = cord[2]
 
     for i in range(cord_x, size + cord_x):
         for j in range(cord_y, size + cord_y):
 
             z = np_img[i, j, layer]
+
             if z == 255:
                 z = 0
                 np_img_qr[i - cord_x, j - cord_y] = z
@@ -83,23 +85,29 @@ def decoder(cord):
     if vertices_array is not None:
         print("QRCode data:", data)
 
-        string_data = "Img_name - " + name_img + " / Result - true / Data - " + data + " / Cord QRCode - " + str(cord) + " \n"
+        string_data = "Img_name - " + name_img + " , Result - true , Data - " + data + " , Cord QRCode - " + str(cord) + " \n"
         file_result.write(string_data)
     else:
         print("There was some error")
-        string_data = 'Img_name - ' + name_img + ' / Result - false / ' + ' / Cord QRCode - ' + str(cord) + ' \n'
+        string_data = 'Img_name - ' + name_img + ' , Result - false , Data - null , Cord QRCode - ' + str(cord) + ' \n'
         file_result.write(string_data)
 
     file_result.close()
     print("end decoder")
 
+def list_img (name_dir):
+    arr = ['./' + name_dir + '/' + x for x in os.listdir('./' + name_dir) if x.endswith(".tiff") or x.endswith(".jpg") or x.endswith(".png") or x.endswith(".tif")]
+    return arr
+
 def test():
-    file_name = 'test_img_name.txt'
-    file_name_img = open(file_name, 'r')
-    name_img = file_name_img.readline()
-    print(name_img)
-    cord = coder(name_img)
-    decoder(cord)
-    file_name_img.close()
+    name_dir = 'image'
+    list_name_img = list_img(name_dir)
+    for i in range(list_name_img.__sizeof__()):
+        print(i)
+        name_img = list_name_img[i]
+        print(name_img)
+        cord = coder(name_img)
+        name_img = './result/result_CMYK' + name_img[8:-3] + 'tif'
+        decoder(cord, name_img)
 
 test()
