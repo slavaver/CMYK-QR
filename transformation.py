c50d0b50ee897205bb68fdc6b910b7b065d46599
Vladislav Vereschagin <slavaver@gmail.com>
2021-11-01 01:26:41 +0300

    Moved profiles to the folder

--- transformation.py
+++ transformation.py
@@ -1,7 +1,8 @@
 from PIL import Image, ImageCms
 
-SRGB = "SRGB.icm"
-CMYK = "CMYK.icc"
+SRGB = "profiles/SRGB.icm"
+CMYK = "profiles/CMYK.icc"
+
 
 def srgb_cmyk(name):
     t = ImageCms.buildTransform(SRGB, CMYK, "RGB", "CMYK")
@@ -10,9 +11,9 @@ def srgb_cmyk(name):
     img_cmyk.save('test_CMYK.tif')
     return img_cmyk
 
+
 def cmyk_srgb(name):
     t = ImageCms.buildTransform(CMYK, SRGB, "CMYK", "RGB")
     img = Image.open(name)
     img_srgb = ImageCms.applyTransform(img, t)
     img_srgb.save('result_sRGB.png')
-
